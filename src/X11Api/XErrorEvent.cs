a883c83b3f4d85ceff7d15d2ca7940884fedff60
Bart <>
2023-09-05 19:10:03 +0200

    Public properties at XErrorEvent

--- src/X11Api/XErrorEvent.cs
+++ src/X11Api/XErrorEvent.cs
@@ -6,11 +6,11 @@ namespace CefNet.Avalonia.X11Api;
 [StructLayout(LayoutKind.Sequential)]
 public struct XErrorEvent
 {
-    internal XEventName type;
-    internal IntPtr display;
-    internal IntPtr resourceid;
-    internal IntPtr serial;
-    internal byte error_code;
-    internal XRequest request_code;
-    internal byte minor_code;
+    public XEventName type;
+    public IntPtr display;
+    public IntPtr resourceid;
+    public IntPtr serial;
+    public byte error_code;
+    public XRequest request_code;
+    public byte minor_code;
 }
